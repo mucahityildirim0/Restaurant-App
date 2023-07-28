a29829b59677542d658a6d7c8ff0d56cb9fa085a
Mücahit Yıldırım <00mucahityildirim@gmail.com>
2023-07-28 13:54:19 +0300

    initialize app and create pages

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MenuPage = () => {
+  return <div>menu Page</div>;
+};
+
+export default MenuPage;
---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,5 @@
+const SingleProductPage = () => {
+  return <div>single product page</div>;
+};
+
+export default SingleProductPage;
